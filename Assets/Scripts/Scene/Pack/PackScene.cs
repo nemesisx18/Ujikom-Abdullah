0cd2f3bc7b260e49f52c1d8b1dd620c613bf7c21
Abdul003 <73937383+nemesisx18@users.noreply.github.com>
2022-09-08 08:27:49 +0800

    Initial commit

---
+++ Assets/Scripts/Scene/Pack/PackScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TriviaGame.Scene.Pack
+{
+    public class PackScene : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
